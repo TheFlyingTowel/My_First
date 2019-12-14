ba4775cd21da26e3d5a9b0e4aacebf427812ce77
Towel <Towel@DESKTOP-2TTUFKP.home>
2019-12-14 18:04:46 -0500

    Desktop

---
+++ desktop/src/com/box/mun/desktop/DesktopLauncher.java
@@ -0,0 +1,13 @@
+package com.box.mun.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.box.mun.BoxMun;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 1080; config.height = 720;
+		new LwjglApplication(new BoxMun(), config);
+	}
+}
